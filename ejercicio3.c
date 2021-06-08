a5e19b9c9b995709c1929c6859729e4db0fdb043
Pauarganaraz <82884412+Pauarganaraz@users.noreply.github.com>
2021-06-08 10:49:03 -0300

    Add files via upload

---
+++ ejercicio3.c
@@ -0,0 +1,26 @@
+#include  <stdio.h>
+
+/*
+Complete las palabras con los caracteres correspondientes para que se imprima en consola lo siguiente:
+	- Esternocleidomastoideo
+	- Paralelepipedo 
+	- Marco de referencia
+*/
+
+int main () {
+	// NO MODIIFICAR
+	char caracterW = ' w ' ;
+	char caracterE = ' e ' ;
+	char caracterI = ' yo ' ;
+	char caracterL = ' l ' ;
+	char caracterR = ' r ' ;
+	char caracterP = ' p ' ;
+	char caracterS = ' s ' ;
+	// NO MODIIFICAR
+
+	printf( " Est % c rnocle % c doma % c toideo \n "  caracterE, caracterI, caracterS);
+	printf( " Para % c ele % c ipedo \n "  caracterL, caracterP);
+	printf( " Marco % c d % c referencia \n "  caracterE);
+
+	return  0 ;
+} 
